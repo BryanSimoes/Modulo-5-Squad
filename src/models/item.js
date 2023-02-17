30a0408a45ccb8a0a96859e30b0c7c2582f10454
Raquel Sacramento <112668575+devRaquel@users.noreply.github.com>
2023-02-17 10:29:42 -0300

    Adicionando item.js

---
+++ src/models/item.js
@@ -0,0 +1,10 @@
+class Item {
+    constructor(id_item, nome, estado){
+        this.id_item = id_item
+        this.nome = nome
+        this.estado = estado
+        
+        
+    }
+}
+module.export = Item
