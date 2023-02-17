21eb8dff229e03e0c613003487d415a1fa63e2e7
Grazielle Firmino <112557569+GraziFirmino@users.noreply.github.com>
2023-02-17 10:22:06 -0300

    adiciona doador models

---
+++ src/models/doador.js
@@ -0,0 +1,10 @@
+class Doador {
+    constructor(id__doador, nome, cpf, email, senha,){
+        this.id__doador = id__doador
+        this.nome = nome
+        this.cpf = cpf
+        this.email = email
+        this.senha = senha 
+    }
+}
+export default  Doador
