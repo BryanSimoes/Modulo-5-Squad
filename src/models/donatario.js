d3c52acb5dede96cb1e9db14d569434949e90ed6
Juliane Prado <112557540+JulianePrado@users.noreply.github.com>
2023-02-17 10:26:48 -0300

    adiciona donatário.js

---
+++ src/models/donatario.js
@@ -0,0 +1,11 @@
+class Donatario {
+    constructor(id_donatario, nome, cpf, email, senha,){
+        this.id_donatario = id_donatario
+        this.nome = nome
+        this.cpf = cpf
+        this.email = email
+        this.senha = senha
+       
+    }
+}
+module.export = Donatario
